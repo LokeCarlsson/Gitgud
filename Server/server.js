187eaea17693da3e33f01bc335862694dd1d6b03
LokeCarlsson <lc222ak@student.lnu.se>
2017-03-08 15:00:48 +0100

    Started working on webhooks

--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,4 @@
+import githubwebhook from 'express-github-webhook'
 import methodOverride from 'method-override'
 import partials from 'express-partials'
 import bodyParser from 'body-parser'
@@ -12,6 +13,8 @@ import headers from './middlewares/headers'
 import { router } from './routes/routes'
 import config from './config/main'
 
+const webhookHandler = githubwebhook({ path: '/webhook', secret: 'secret' });
+
 let app = express()
 app.server = http.createServer(app)
 
@@ -20,6 +23,7 @@ mongoose.connect(config.database)
 app.use(cors({
 	exposedHeaders: config.corsHeaders
 }))
+app.use(webhookHandler)
 app.use(headers)
 app.use(partials())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -28,6 +32,12 @@ app.use(methodOverride())
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(express.static(__dirname + '/public'))
+
+webhookHandler.on('*', function (event, repo, data) {
+	console.log('event - ', event)
+	console.log('repo - ', repo)
+	console.log('data - ', data)
+})
 
 app.use(router)
 
