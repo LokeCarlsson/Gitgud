b2be3547c64a1dc5a33f5b69b1391315d62d643d
LokeCarlsson <lc222ak@student.lnu.se>
2017-03-09 18:28:20 +0100

    Added timeline

--- Client/src/components/info/Start.vue
+++ Client/src/components/info/Start.vue
@@ -1,34 +1,67 @@
 <template>
   <div id='start'>
-    <h4>
-      Hello!
-    </h4>
-    <p>
-      Welcome to Gitgud!
-    </p>
-    <p>
-      Use timelines:
-      <a href="http://quasar-framework.org/components/timeline.html">quasar-framework.org/components/timeline</a>
-    </p>
-    <div> {{ github }}</div>
+    <div v-show="this.authenticated" class="timeline">
+      <div class="timeline-label">
+        <h4 class="bg-white text-italic">
+          Today
+        </h4>
+      </div>
+
+        <div class="timelineEvents" v-for="event in github">
+          <div class="timeline-item">
+            <div class="timeline-badge">
+              <img :src="event.actor.avatar_url">
+            </div>
+
+          <div class="timeline-title">
+            {{ event.actor.login }}
+          </div>
+
+          <div class="timeline-date text-italic">
+            <div>{{ event.created_at }}</div>
+          </div>
+
+          <div class="card">
+            <div class="card-title">
+              {{ event.repo.name }}
+            </div>
+
+            <div class="card-content">
+              <div v-for="commit in event.payload.commits" class="timeline-item">
+                {{ commit.message }}
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <script>
+  import auth from '../auth/index.js'
   export default {
     data () {
       return {
-        github: 'There will be stuff here!'
+        github: [],
+        authenticated: auth.user.authenticated
       }
     },
     mounted () {
-
+      // this.axios.get('https://api.github.com/users/' + localStorage.getItem('username') + '/events')
+      this.axios.get('https://api.github.com/orgs/wp15/events')
+        .then((payload) => {
+          console.log(payload)
+          this.github = payload.data
+        })
     }
   }
 
 </script>
 
 <style>
-
 
 </style>
