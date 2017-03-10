a86ac7de78847bba4c4730d694cd2a5952d2b2f3
LokeCarlsson <lc222ak@student.lnu.se>
2017-03-10 18:06:32 +0100

    Cleaning up old code

--- Client/src/components/auth/index.js
+++ Client/src/components/auth/index.js
@@ -10,41 +10,6 @@ export default {
     store.dispatch('removeToken')
     store.dispatch('removeUsername')
   },
-  checkAuth () {
-    // this.axios.get('/account', { headers: this.getAuthHeader() })
-    //   .then((payload) => {
-    //     this.user.username = payload.data.username
-    //     this.user.authenticated = true
-    //     return true
-    //   })
-    //   .catch((e) => {
-    //     this.user.authenticated = false
-    //     console.log(e)
-    //     this.logout()
-    //     return false
-    //   })
-  },
-  checkToken () {
-    console.log('logged in? ', store.getters.authenticated)
-    // const jwt = localStorage.getItem('token')
-    // if (jwt !== undefined && jwt !== null && jwt.length > 0) {
-    //   this.user.authenticated = true
-    //   return true
-    // }
-    // else {
-    //   this.user.authenticated = false
-    //   return false
-    // }
-  },
-  isLoggedIn () {
-    return store.getters.authenticated
-  },
-  getToken () {
-    return store.getters.token
-  },
-  getUser () {
-    return store.getters.user
-  },
   getAuthHeader () {
     return {
       'Authorization': localStorage.getItem('token')
