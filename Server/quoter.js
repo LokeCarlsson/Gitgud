9c556e53c71a018020555c2e2f4a2dc5d9ee03d1
LokeCarlsson <lc222ak@student.lnu.se>
2017-03-13 16:37:18 +0100

    Removed old unused files

--- Server/quoter.js
+++
@@ -1,7 +0,0 @@
-import quotes from './quotes'
-
-export default function getRandom() {
-  let totalAmount = quotes.length
-  let rand = Math.floor(Math.random() * totalAmount)
-  return quotes[rand]
-}
--- Server/statusError.js
+++
@@ -1,8 +0,0 @@
-function StatusError(msg, status) {
-    var err = Error.call(this, msg)
-    err.status = status
-    err.name = 'StatusError'
-    return err
-}
-
-export default StatusError
