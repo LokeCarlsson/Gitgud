270d46485966208c6d2655f9955985945b0abf05
LokeCarlsson <lc222ak@student.lnu.se>
2017-03-14 04:10:36 +0100

    Minor changes

--- Client/src/components/timelines/User.vue
+++ Client/src/components/timelines/User.vue
@@ -6,26 +6,21 @@
           Today
         </h4>
       </div>
-
         <div class="timelineEvents" v-for="event in github">
           <div class="timeline-item">
             <div class="timeline-badge">
               <img :src="event.actor.avatar_url">
             </div>
-
           <div class="timeline-title">
             {{ event.actor.login }}
           </div>
-
           <div class="timeline-date text-italic">
             <div>{{ event.created_at }}</div>
           </div>
-
           <div class="card">
             <div class="card-title">
               {{ event.repo.name }}
             </div>
-
             <div class="card-content">
               <div v-for="commit in event.payload.commits" class="timeline-item">
                 {{ commit.message }}
@@ -33,11 +28,8 @@
             </div>
           </div>
         </div>
-
       </div>
-
     </div>
-
   </div>
 </template>
 
